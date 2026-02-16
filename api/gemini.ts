a79f36a77e2a5a87b4376aa9a1e7d18c78ad4583
shubhamg149 <smartyshubham321@gmail.com>
2026-02-16 15:53:41 +0530

    Update gemini.ts

--- api/gemini.ts
+++ api/gemini.ts
@@ -17,7 +17,7 @@ export default async function handler(
     }
 
     const ai = new GoogleGenAI({
-      apiKey: process.env.GEMINI_API_KEY||,
+      apiKey: process.env.GEMINI_API_KEY,
     });
 
     const response = await ai.models.generateContent({
